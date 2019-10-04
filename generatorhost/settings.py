bcf5833f719195bde17822fcb5090cced5d1f115
JackTheWright <jackwrightwork@gmail.com>
2019-10-03 19:32:38 -0700

    changed staticdirs and html

--- generatorhost/settings.py
+++ generatorhost/settings.py
@@ -119,4 +119,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = {
+        os.path.join(BASE_DIR, 'generatorhost/static'),
+}
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
