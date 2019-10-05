fdc061b84287fcf2940352954540987f700e82ab
JackTheWright <jackwrightwork@gmail.com>
2019-10-05 12:10:02 -0700

    took out root and only using dir

--- generatorhost/settings.py
+++ generatorhost/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'character')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'character')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
