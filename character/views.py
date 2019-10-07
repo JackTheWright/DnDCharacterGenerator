f4596b40b1f636e8573aced4b5918fb84251fdc2
JackTheWright <jackwrightwork@gmail.com>
2019-10-07 10:35:58 -0700

    still working with paths

--- character/views.py
+++ character/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.contrib.staticfiles.finders import find
 
 from . import poppdf
 
 def character(request):
-    url1 = static('/character/charactersheet.pdf')
-    url2 = static('/character/newcharactersheet.pdf')
+    url1 = find('/character/charactersheet.pdf')
+    url2 = find('/character/newcharactersheet.pdf')
     poppdf.write_fillable_pdf(url1,
                               url2,
                               poppdf.data_dict)
