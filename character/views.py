7935883764509678d3d8bfda3a0197e8bb711ceb
JackTheWright <jackwrightwork@gmail.com>
2019-10-07 10:22:41 -0700

    changed path in views.py

--- character/views.py
+++ character/views.py
@@ -4,7 +4,7 @@ from . import poppdf
 
 def character(request):
     url1 = '/static/character/charactersheet.pdf'
-    url2 = staticfiles_storage.url('character/newcharactersheet.pdf')
+    url2 = '/static/character/newcharactersheet.pdf'
     poppdf.write_fillable_pdf(url1,
                               url2,
                               poppdf.data_dict)
