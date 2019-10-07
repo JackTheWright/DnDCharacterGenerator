23a7a7a80f6a1754cddbe8e3a74342315baa9496
JackTheWright <jackwrightwork@gmail.com>
2019-10-07 16:00:52 -0700

    path issues on heroku side

--- character/views.py
+++ character/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.staticfiles.templatetags.staticfiles import static
+import os
 
 from . import poppdf
 
