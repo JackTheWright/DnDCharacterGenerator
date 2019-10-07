9f53700bc4734e2193db15bb0163b700829002a6
JackTheWright <jackwrightwork@gmail.com>
2019-10-07 01:00:03 -0700

    added pdf generator

--- character/views.py
+++ character/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import poppdf
 
 def character(request):
-   return render(request, 'character.html', {}) 
+    poppdf.write_fillable_pdf(poppdf.CHARSHEET_PATH,
+                              poppdf.CHARSHEET_OUTPUT_PATH,
+                              poppdf.data_dict)
+    return render(request, 'character.html', {})
 # Create your views here.
