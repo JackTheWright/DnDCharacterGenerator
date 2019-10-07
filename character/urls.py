0d2ae29237f225e6ddc37ffcbd4b0f0e763b007d
JackTheWright <jackwrightwork@gmail.com>
2019-10-07 16:55:58 -0700

    fixed pathing, splitting urls

--- character/urls.py
+++ character/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-        path('/generate', views.character, name='character'),
-        path('/download', views.download, name='download'),
+        path('/generate/', views.character, name='character'),
+        path('', views.download, name='download'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
