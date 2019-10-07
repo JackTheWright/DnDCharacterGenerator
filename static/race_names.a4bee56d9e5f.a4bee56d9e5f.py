d4e51da5c9a55ecc7c367662af8b1c01e0a69771
JackTheWright <jackwrightwork@gmail.com>
2019-10-07 12:50:08 -0700

    fixing paths

---
+++ static/race_names.a4bee56d9e5f.a4bee56d9e5f.py
@@ -0,0 +1,66 @@
+# file with list of names
+class names: 
+    human_male = ['Anlow', 'Arando', 'Bram', 'Cale', 'Dalkon', 'Daylen', 'Dodd',\
+     'Dungarth', 'Dyrk', 'Eandro', 'Falken', 'Feck', 'Fenton', 'Gryphero', 'Hagar',\
+     'Jeras', 'Krynt', 'Lavant', 'Leyten', 'Madian', 'Malfier', 'Markus', 'Meklan',\
+     'Namen', 'Navaren', 'Nerle', 'Nilus', 'Ningyan', 'Norris', 'Quentin', 'Semil',\
+     'Sevenson' , 'Steveren', 'Talfen', 'Tamond', 'Taran', 'Tavon', 'Tegan',\
+     'Vanan', 'Vincent']
+
+    human_female = ['Azura', 'Brey', 'Hallan', 'Kasaki', 'Lorelei', 'Mirabel',\
+     'Pharana', 'Remora', 'Rosalyn', 'Sachil', 'Saidi', 'Tanika', 'Tura', 'Tylsa',\
+     'Vencia', 'Xandrilla']
+
+    dragonborn_male = ['Andujar', 'Armagan', 'Armek', 'Arzan', 'Axaran',\
+     'Belaxarim', 'Brevarr', 'Djemidor', 'Draxan', 'Fayal', 'Grax', 'Iojad',\
+     'Inzul', 'Khiraj', 'Kreytzen', 'Lejek', 'Mar', 'Nazir', 'Nedam', 'Nevek',\
+     'Ravaran', 'Razaan', 'Sarax', 'Sarram', 'Savaxis', 'Siangar', 'Sirizan',\
+     'Sunan', 'Szuran', 'Tajan', 'Tamajon', 'Tenahn', 'Toxal', 'Tzegyr',\
+     'Vantajar', 'Vharkus', 'Xafiq', 'Zarkhil']
+
+    dragonborn_female = ['Artana', 'Kalas', 'Khagra', 'Leytra', 'Myrka', 'Naya',\
+     'Sarcha', 'Shirren', 'Sirivistra', 'Sufana', 'Tamara', 'Vrumadi', 'Zovra']
+
+    dwarf_male = ['Agaro', 'Arnan', 'Auxlan', 'Avamir', 'Baelnar', 'Balfam',\
+     'Bariken', 'Bork\'û\'l', 'Dark\'û\'l', 'Dolmen', 'Dyrnar', 'Erag', 'Ezegan',\
+     'Ferrek', 'Garm\'û\'l', 'Glint', 'Ghorvas', 'Grimmalk', 'Haeltar',\
+     'Halagmar', 'Halzar', 'Hlant', 'Korlag', 'Krag', 'Krim', 'Kurman', 'Lurtrum',\
+     'Malagar', 'Mardam', 'Maulnar', 'Melgar', 'Morak', 'Orobok', 'Rogath',\
+     'Roken', 'Rozag', 'Sabakzar', 'Sharak', 'Smethykk', 'Swargar', 'Thorbalt',\
+     'Thorin', 'Tredigar', 'Vab\'û\'l', 'Vistrum', 'Wolvar']
+
+    dwarf_female = ['Beyla', 'Fenryl', 'Grenenzel', 'Krystolari', 'Lokara',\
+     'Lurka', 'Marnia', 'Praxana', 'Rokel', 'Roksana', 'Thurlfara', 'Vauldra',\
+     'Veklani', 'Vronwe', 'Zebel']
+
+    elf_male = ['Alarcion', 'Alathar', 'Ariandar', 'Arromar', 'Borel', 'Bvachan',\
+     'Carydion', 'Elgoth', 'Farlien', 'Ferel', 'Gaerlan', 'Iafalior', 'Kaelthorn',\
+     'Laethan', 'Leliar', 'Leodor', 'Lorak', 'Lorifir', 'Morian', 'Oleran',\
+     'Rylef', 'Savian', 'Seylas', 'Tevior', 'Veyas']
+
+    elf_female = ['Aryllan', 'Atalya', 'Ayrthwil', 'Irva', 'Lyfalia', 'Ronefel',\
+     'Thirya', 'Velene', 'Venefiq', 'Zereni']
+
+    halfling_male = ['Arthan', 'Carvin', 'Corby', 'Cullen', 'Egen', 'Ernest',\
+     'Gedi', 'Heron', 'Jeryl', 'Keffen', 'Kylem', 'Kynt', 'Leskyn', 'Neff', 'Orne',\
+     'Quarrel', 'Rabbit', 'Rilkin', 'Snakebait', 'Tarfen', 'Titch', 'Tuck', 'Whim']
+
+    halfling_female = ['Caliope', 'Emily', 'Piper', 'Rixi', 'Sabretha', 'Teg',\
+     'Tilly', 'Toira', 'Vexia', 'Vil', 'Vzani', 'Zanthe', 'Ziza']
+
+    tiefling_male = ['Ankhus', 'Arkadi', 'Armarius', 'Armillius', 'Archidius',\
+      'Balmoloch', 'Calderax', 'Cavian', 'Cenereth', 'Chorum', 'Corynax', 'Dacian',\
+      'Daelius', 'Damaceus', 'Decimeth', 'Demedor', 'Demerian', 'Dynachus',\
+      'Grassus', 'Halius', 'Heleph', 'Incirion', 'Kalaradian', 'Kamien', 'Kazimir',\
+      'Kzandro', 'Machem', 'Maetheus', 'Malfias', 'Marchion', 'Menerus',\
+      'Namazeus', 'Nensis', 'Prismeus', 'Pyranikus', 'Razortail', 'Sejanus',\
+      'Severian', 'Suffer', 'Syken', 'Tarkus', 'Vaius', 'Xerek', 'Zeth', 'Zevon']
+
+    tiefling_female = ['Affyria', 'Cataclysmia', 'Domitia', 'Dorethau',\
+     'Excellence', 'Hacari', 'Iritra', 'Lachira', 'Levatra', 'Mecretia', 'Milvia',\
+     'Nericia', 'Precious', 'Rain', 'Samantia', 'Sunshine', 'Tenerife', 'Traya',\
+     'Velavia', 'Zaidi', 'Zethaya']
+
+    male_names = [dwarf_male, elf_male, halfling_male, human_male, dragonborn_male, tiefling_male]
+
+    female_names = [dwarf_female, elf_female, halfling_female, human_female, dragonborn_female, tiefling_female]
