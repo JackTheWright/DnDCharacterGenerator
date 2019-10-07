62f8d34110663227369fdcba80d0ec8418737db7
JackTheWright <jackwrightwork@gmail.com>
2019-10-07 10:21:14 -0700

    changed path in views.py

--- character/views.py
+++ character/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.staticfiles.storage import staticfiles_storage
 from . import poppdf
 
 def character(request):
-    url1 = staticfiles_storage.url('character/charactersheet.pdf')
+    url1 = '/static/character/charactersheet.pdf'
     url2 = staticfiles_storage.url('character/newcharactersheet.pdf')
     poppdf.write_fillable_pdf(url1,
                               url2,
