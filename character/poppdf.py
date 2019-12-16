5fd75c4596646e1147b5e78f057500cf0dfa3bac
JackTheWright <jackwrightwork@gmail.com>
2019-12-16 00:50:43 -0800

    Fixed Scope Issue

--- character/poppdf.py
+++ character/poppdf.py
@@ -36,6 +36,7 @@ def dictfiller(level):
     percepflag = 0
     ability_score_improvements = 0
     allowed_spells = 0
+    class_features = ''
     spell_slots = {'c': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0,
                     '7':0, '8':0, '9':0}
 
