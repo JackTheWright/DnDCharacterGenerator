fc06e26b2b578eb7b27763fe126363eb20c7e7ca
JackTheWright <jackwrightwork@gmail.com>
2019-12-16 12:12:25 -0800

    trying to get GET message

--- character/views.py
+++ character/views.py
@@ -8,7 +8,7 @@ from . import poppdf
 def generate(request):
     url1 = 'static/charactersheet.pdf'
     url2 = 'static/character/newcharactersheet.pdf'
-    print(request.GET.items())
+    print(request.body)
     poppdf.write_fillable_pdf(url1,
                               url2,
                               poppdf.data_dict, 5)
